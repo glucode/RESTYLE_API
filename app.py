cefaef1ae129a20b92b7cb15057c56789ee572c0
patglu <pat@glucode.com>
2023-10-20 22:34:30 +0200

    Add image segmentation functionality and save segmented images
    Changes Made:
    - Created the `segment_image` function to perform image segmentation.
    - Modified the `output_path` to save segmented images with the original file name.
    - Added error handling for invalid file uploads in the Flask route.
    
    This functionality allows users to upload an image and receive the corresponding segmented image as a response.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 import requests
 from external_Files import generator 
 from external_Files import clasification
+from external_Files import transcoder
 import firebase_admin
 from firebase_admin import credentials, firestore
 
@@ -49,6 +50,34 @@ def generate_html_list():
     output = generator.Generator.start_genertation_html(name)
     return output,200
 
+@app.route('/remove_background',methods=['POST'])
+def remove_background():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
+        file = request.files['image']
+
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        # Save the uploaded image to a temporary file
+        upload_folder = "uploads"
+        os.makedirs(upload_folder, exist_ok=True)
+        temp_image_path = os.path.join(upload_folder, file.filename)
+        file.save(temp_image_path)
+
+        # Call the segment_image function with the saved file path
+        segmented_image = transcoder.segment_image(temp_image_path)
+
+        # Optionally, you can delete the temporary file if you no longer need it
+        os.remove(temp_image_path)
+
+        return jsonify({'segmented_image': "success"}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+   
 
 @app.route('/not_found')
 def not_fount():
--- external_Files/transcoder.py
+++ external_Files/transcoder.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import numpy as np
 import cv2
 import torch
@@ -6,42 +5,50 @@ import albumentations as albu
 from iglovikov_helper_functions.utils.image_utils import load_rgb, pad, unpad
 from iglovikov_helper_functions.dl.pytorch.utils import tensor_from_rgb_image
 from cloths_segmentation.pre_trained_models import create_model
-import matplotlib.pyplot as plt
+import os 
 
-# Load the pre-trained model and set it to evaluation mode
-model = create_model("Unet_2020-10-30")
-model.eval()
+def segment_image(image_path):
+    # Ensure that the provided argument is a FileStorage object
+    # if not isinstance(image_path, FileStorage):
+    #     raise ValueError("Expected a FileStorage object.")
 
-# Load the image
-image = load_rgb("IMG_1160 2.png")
+    # Load the pre-trained model and set it to evaluation mode
+    model = create_model("Unet_2020-10-30")
+    model.eval()
 
-# Define a normalization transformation
-transform = albu.Compose([albu.Normalize(p=1)], p=1)
+    # Load the image from the FileStorage object
+    image = load_rgb(image_path)
 
-# Pad the image to make its dimensions divisible by 32
-padded_image, pads = pad(image, factor=32, border=cv2.BORDER_CONSTANT)
 
-# Convert the padded image to a tensor format suitable for the model
-x = transform(image=padded_image)["image"]
-x = torch.unsqueeze(tensor_from_rgb_image(x), 0)
+    # Define a normalization transformation
+    transform = albu.Compose([albu.Normalize(p=1)], p=1)
 
-# Get the segmentation mask from the model
-with torch.no_grad():
-    prediction = model(x)[0][0]
+    # Pad the image to make its dimensions divisible by 32
+    padded_image, pads = pad(image, factor=32, border=cv2.BORDER_CONSTANT)
 
-# Convert the prediction to a binary mask and remove padding
-mask = (prediction > 0).cpu().numpy().astype(np.uint8)
-mask = unpad(mask, pads)
+    # Convert the padded image to a tensor format suitable for the model
+    x = transform(image=padded_image)["image"]
+    x = torch.unsqueeze(tensor_from_rgb_image(x), 0)
 
-mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    # Get the segmentation mask from the model
+    with torch.no_grad():
+        prediction = model(x)[0][0]
 
-segmented_image = image * mask_rgb
+    # Convert the prediction to a binary mask and remove padding
+    mask = (prediction > 0).cpu().numpy().astype(np.uint8)
+    mask = unpad(mask, pads)
 
-plt.imshow(segmented_image)
-plt.show()
+    mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
 
-# # Save the segmented image as a PNG
-# output_path = f"{image}.png"
-# cv2.imwrite(output_path, cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR))
-# print(f"Segmented image saved to {output_path}")
+    segmented_image = image * mask_rgb
+    
+    # temp_image_path = os.path.join(upload_folder, segmented_image.filename)
+    # save_path ="uploads" 
+
+    #     # Save the segmented image as a PNG
+   # Save the segmented image as a PNG
+    output_filename = os.path.basename(image_path)  # Get the original image file name
+    output_path = os.path.join("uploads", f"{output_filename}_no_background.png")
+    cv2.imwrite(output_path, cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR))
+    print(f"Image saved as {output_path}")
 
