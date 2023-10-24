389306330a14874116cb4f0eca1190769cb81d7b
patglu <pat@glucode.com>
2023-10-24 13:03:53 +0200

    classy women category
    The classy preset has been added for women

--- external_Files/presets.py
+++ external_Files/presets.py
@@ -19,50 +19,50 @@ COLOR_WHEEL = {
 class GenerateOutfitsConfig:
 
     COLOR_PROFILES = {
-    "Triads": {
-        "Red": ["Blue-Green", "Yellow-Green"],
-        "Red-Orange": ["Blue", "Green"],
-        "Orange": ["Blue-Purple", "Yellow-Green"],
-        "Yellow-Orange": ["Purple", "Blue-Green"],
-        "Yellow": ["Blue", "Red-Purple"],
-        "Yellow-Green": ["Red", "Blue-Purple"],
-        "Green": ["Red", "Blue"],
-        "Blue-Green": ["Red-Orange", "Red-Purple"],
-        "Blue": ["Red-Orange", "Yellow-Orange"],
-        "Blue-Purple": ["Yellow", "Green"],
-        "Purple": ["Yellow", "Blue-Green"],
-        "Red-Purple": ["Yellow-Green", "Blue"]
-    },
-    "Complementary": {
-        "Red": ["Red-Orange", "Red-Purple", "Blue-Green", "Yellow-Green"],
-        "Red-Orange": ["Red", "Orange", "Blue-Purple", "Green"],
-        "Orange": ["Red-Orange", "Yellow-Orange", "Blue", "Purple"],
-        "Yellow-Orange": ["Orange", "Yellow", "Blue-Green", "Red-Purple"],
-        "Yellow": ["Yellow-Orange", "Yellow-Green", "Purple", "Red"],
-        "Yellow-Green": ["Yellow", "Green", "Red-Purple", "Red-Orange"],
-        "Green": ["Yellow-Green", "Blue-Green", "Red", "Red-Orange"],
-        "Blue-Green": ["Green", "Blue", "Red-Orange", "Yellow-Orange"],
-        "Blue": ["Blue-Green", "Blue-Purple", "Orange", "Yellow"],
-        "Blue-Purple": ["Blue", "Purple", "Yellow-Orange", "Green"],
-        "Purple": ["Blue-Purple", "Red-Purple", "Yellow", "Blue"],
-        "Red-Purple": ["Purple", "Red", "Green", "Yellow-Green"],
-    },
-    "Monotone": {color: [color] for color in COLOR_WHEEL.keys()},
-    "Analogous": {
-        "Red": ["Red-Orange", "Red-Purple"],
-        "Red-Orange": ["Red", "Orange"],
-        "Orange": ["Red-Orange", "Yellow-Orange"],
-        "Yellow-Orange": ["Orange", "Yellow"],
-        "Yellow": ["Yellow-Orange", "Yellow-Green"],
-        "Yellow-Green": ["Yellow", "Green"],
-        "Green": ["Yellow-Green", "Blue-Green"],
-        "Blue-Green": ["Green", "Blue"],
-        "Blue": ["Blue-Green", "Blue-Purple"],
-        "Blue-Purple": ["Blue", "Purple"],
-        "Purple": ["Blue-Purple", "Red-Purple"],
-        "Red-Purple": ["Purple", "Red"]
+        "Triads": {
+            "Red": ["Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Blue", "Green"],
+            "Orange": ["Blue-Purple", "Yellow-Green"],
+            "Yellow-Orange": ["Purple", "Blue-Green"],
+            "Yellow": ["Blue", "Red-Purple"],
+            "Yellow-Green": ["Red", "Blue-Purple"],
+            "Green": ["Red", "Blue"],
+            "Blue-Green": ["Red-Orange", "Red-Purple"],
+            "Blue": ["Red-Orange", "Yellow-Orange"],
+            "Blue-Purple": ["Yellow", "Green"],
+            "Purple": ["Yellow", "Blue-Green"],
+            "Red-Purple": ["Yellow-Green", "Blue"]
+        },
+        "Complementary": {
+            "Red": ["Red-Orange", "Red-Purple", "Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Red", "Orange", "Blue-Purple", "Green"],
+            "Orange": ["Red-Orange", "Yellow-Orange", "Blue", "Purple"],
+            "Yellow-Orange": ["Orange", "Yellow", "Blue-Green", "Red-Purple"],
+            "Yellow": ["Yellow-Orange", "Yellow-Green", "Purple", "Red"],
+            "Yellow-Green": ["Yellow", "Green", "Red-Purple", "Red-Orange"],
+            "Green": ["Yellow-Green", "Blue-Green", "Red", "Red-Orange"],
+            "Blue-Green": ["Green", "Blue", "Red-Orange", "Yellow-Orange"],
+            "Blue": ["Blue-Green", "Blue-Purple", "Orange", "Yellow"],
+            "Blue-Purple": ["Blue", "Purple", "Yellow-Orange", "Green"],
+            "Purple": ["Blue-Purple", "Red-Purple", "Yellow", "Blue"],
+            "Red-Purple": ["Purple", "Red", "Green", "Yellow-Green"],
+        },
+        "Monotone": {color: [color] for color in COLOR_WHEEL.keys()},
+        "Analogous": {
+            "Red": ["Red-Orange", "Red-Purple"],
+            "Red-Orange": ["Red", "Orange"],
+            "Orange": ["Red-Orange", "Yellow-Orange"],
+            "Yellow-Orange": ["Orange", "Yellow"],
+            "Yellow": ["Yellow-Orange", "Yellow-Green"],
+            "Yellow-Green": ["Yellow", "Green"],
+            "Green": ["Yellow-Green", "Blue-Green"],
+            "Blue-Green": ["Green", "Blue"],
+            "Blue": ["Blue-Green", "Blue-Purple"],
+            "Blue-Purple": ["Blue", "Purple"],
+            "Purple": ["Blue-Purple", "Red-Purple"],
+            "Red-Purple": ["Purple", "Red"]
+        }
     }
-}
     
     # Color Temperatures
     WARM_COLORS = ["Red", "Red-Orange", "Orange", "Yellow-Orange", "Yellow"]
@@ -96,7 +96,7 @@ class WomenStyles(Enum):
     CLASSY = "classy"
     
     
-class WomensConfig(GenerateOutfitsConfig):
+class WomensBohemianConfig(GenerateOutfitsConfig):
 
     COLOR_PROFILES = {
         "Triads": {
@@ -227,35 +227,35 @@ class WomensStreetwearConfig(GenerateOutfitsConfig):
 class WomensRomanticConfig(GenerateOutfitsConfig):
         
     COLOR_PROFILES = {
-    "Triads": {
-        "Red": ["Blue-Green", "Yellow-Green"],
-        "Red-Orange": ["Blue", "Green"],
-        "Orange": ["Blue-Purple", "Yellow-Green"],
-        "Yellow-Orange": ["Purple", "Blue-Green"],
-        "Yellow": ["Blue", "Red-Purple"],
-        "Yellow-Green": ["Red", "Blue-Purple"],
-        "Green": ["Red", "Blue"],
-        "Purple": ["Yellow", "Blue-Green"],
-        "Red-Purple": ["Yellow-Green", "Blue"]
-    },
-    "Complementary": {
-        "Red": ["Red-Orange", "Red-Purple", "Blue-Green", "Yellow-Green"],
-        "Red-Orange": ["Red", "Orange", "Blue-Purple", "Green"],
-        "Orange": ["Red-Orange", "Yellow-Orange", "Blue", "Purple"],
-        "Yellow-Orange": ["Orange", "Yellow", "Blue-Green", "Red-Purple"],
-        "Blue-Purple": ["Blue", "Purple", "Yellow-Orange", "Green"],
-        "Purple": ["Blue-Purple", "Red-Purple", "Yellow", "Blue"],
-        "Red-Purple": ["Purple", "Red", "Green", "Yellow-Green"],
-    },
-    "Monotone": {color: [color] for color in COLOR_WHEEL.keys()},
-    "Analogous": {
-        "Red": ["Red-Orange", "Red-Purple"],
-        "Red-Orange": ["Red", "Orange"],
-        "Orange": ["Red-Orange", "Yellow-Orange"],
-        "Purple": ["Blue-Purple", "Red-Purple"],
-        "Red-Purple": ["Purple", "Red"]
+        "Triads": {
+            "Red": ["Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Blue", "Green"],
+            "Orange": ["Blue-Purple", "Yellow-Green"],
+            "Yellow-Orange": ["Purple", "Blue-Green"],
+            "Yellow": ["Blue", "Red-Purple"],
+            "Yellow-Green": ["Red", "Blue-Purple"],
+            "Green": ["Red", "Blue"],
+            "Purple": ["Yellow", "Blue-Green"],
+            "Red-Purple": ["Yellow-Green", "Blue"]
+        },
+        "Complementary": {
+            "Red": ["Red-Orange", "Red-Purple", "Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Red", "Orange", "Blue-Purple", "Green"],
+            "Orange": ["Red-Orange", "Yellow-Orange", "Blue", "Purple"],
+            "Yellow-Orange": ["Orange", "Yellow", "Blue-Green", "Red-Purple"],
+            "Blue-Purple": ["Blue", "Purple", "Yellow-Orange", "Green"],
+            "Purple": ["Blue-Purple", "Red-Purple", "Yellow", "Blue"],
+            "Red-Purple": ["Purple", "Red", "Green", "Yellow-Green"],
+        },
+        "Monotone": {color: [color] for color in COLOR_WHEEL.keys()},
+        "Analogous": {
+            "Red": ["Red-Orange", "Red-Purple"],
+            "Red-Orange": ["Red", "Orange"],
+            "Orange": ["Red-Orange", "Yellow-Orange"],
+            "Purple": ["Blue-Purple", "Red-Purple"],
+            "Red-Purple": ["Purple", "Red"]
+        }
     }
-}
     
         # Color Temperatures
     WARM_COLORS = ["Red", "Red-Orange", "Orange", "Yellow-Orange", "Yellow"]
@@ -273,3 +273,69 @@ class WomensRomanticConfig(GenerateOutfitsConfig):
     MAX_OUTFITS = 15
     TOP_N_ITEMS_TO_SORT = 2
     PRIORITY_RANDOM_SELECTION = 0.7
+    
+class WomensClassyConfig(GenerateOutfitsConfig):
+        
+    COLOR_PROFILES = {
+        "Triads": {
+            "Red": ["Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Blue", "Green"],
+            "Orange": ["Blue-Purple", "Yellow-Green"],
+            "Yellow-Orange": ["Purple", "Blue-Green"],
+            "Yellow": ["Blue", "Red-Purple"],
+            "Yellow-Green": ["Red", "Blue-Purple"],
+            "Green": ["Red", "Blue"],
+            "Blue-Green": ["Red-Orange", "Red-Purple"],
+            "Blue": ["Red-Orange", "Yellow-Orange"],
+            "Blue-Purple": ["Yellow", "Green"],
+            "Purple": ["Yellow", "Blue-Green"],
+            "Red-Purple": ["Yellow-Green", "Blue"]
+        },
+        "Complementary": {
+            "Red": ["Red-Orange", "Red-Purple", "Blue-Green", "Yellow-Green"],
+            "Red-Orange": ["Red", "Orange", "Blue-Purple", "Green"],
+            "Orange": ["Red-Orange", "Yellow-Orange", "Blue", "Purple"],
+            "Yellow-Orange": ["Orange", "Yellow", "Blue-Green", "Red-Purple"],
+            "Yellow": ["Yellow-Orange", "Yellow-Green", "Purple", "Red"],
+            "Yellow-Green": ["Yellow", "Green", "Red-Purple", "Red-Orange"],
+            "Green": ["Yellow-Green", "Blue-Green", "Red", "Red-Orange"],
+            "Blue-Green": ["Green", "Blue", "Red-Orange", "Yellow-Orange"],
+            "Blue": ["Blue-Green", "Blue-Purple", "Orange", "Yellow"],
+            "Blue-Purple": ["Blue", "Purple", "Yellow-Orange", "Green"],
+            "Purple": ["Blue-Purple", "Red-Purple", "Yellow", "Blue"],
+            "Red-Purple": ["Purple", "Red", "Green", "Yellow-Green"],
+        },
+        "Monotone": {color: [color] for color in COLOR_WHEEL.keys()},
+        "Analogous": {
+            "Red": ["Red-Orange", "Red-Purple"],
+            "Red-Orange": ["Red", "Orange"],
+            "Orange": ["Red-Orange", "Yellow-Orange"],
+            "Yellow-Orange": ["Orange", "Yellow"],
+            "Yellow": ["Yellow-Orange", "Yellow-Green"],
+            "Yellow-Green": ["Yellow", "Green"],
+            "Green": ["Yellow-Green", "Blue-Green"],
+            "Blue-Green": ["Green", "Blue"],
+            "Blue": ["Blue-Green", "Blue-Purple"],
+            "Blue-Purple": ["Blue", "Purple"],
+            "Purple": ["Blue-Purple", "Red-Purple"],
+            "Red-Purple": ["Purple", "Red"]
+        }
+    }
+    
+        # Color Temperatures
+    WARM_COLORS = ["Red", "Red-Orange", "Orange", "Yellow-Orange", "Yellow"]
+    COOL_COLORS = ["Yellow-Green", "Green", "Blue-Green", "Blue", "Blue-Purple", "Purple", "Red-Purple"]
+
+    # Base Category Constraints
+
+    base_category_constraints = {
+        "tops": ["polo", "shirts", "jumpsuits", "sweaters","hoodie"],
+        "trousers": ["pants", "trousers", ],
+        "shoes": ["boots", "heels"]
+    }
+    
+    # Other configurations
+    MAX_OUTFITS = 15
+    TOP_N_ITEMS_TO_SORT = 2
+    PRIORITY_RANDOM_SELECTION = 0.7
+    
